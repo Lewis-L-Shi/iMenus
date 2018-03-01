c332a41380778ce4c079f4e06666aefe44b0d2b9
Lewis <yishil@uw.edu>
2018-02-28 19:52:57 -0800

    Moved Boot Code to AppDelegate, Changed Custom Geolocation

--- iMenus/Controllers/ViewController.swift
+++ iMenus/Controllers/ViewController.swift
@@ -8,11 +8,9 @@
 
 import UIKit
 import SQLite
-import CoreLocation
-import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
-    let locationManager = CLLocationManager()
+class ViewController: UIViewController {
+    
     @IBOutlet weak var welcomeLabel: UILabel!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var logoutButton: UIButton!
@@ -20,18 +18,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        copyDatabaseIfNeeded()
         
-        self.locationManager.requestAlwaysAuthorization()
-        
-        // For use in foreground
-        self.locationManager.requestWhenInUseAuthorization()
-        
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager.delegate = self
-            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-            locationManager.startUpdatingLocation()
-        }
         logoutButton.isHidden = true
         loginButton.isHidden = false
         let uName = UserDefaults.standard.value(forKey: "username") as? String
@@ -40,7 +27,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             logoutButton.isHidden = false
             welcomeLabel.text = "Welcome \(uName ?? "Guest")!"
         }
-        
     }
 
     override func didReceiveMemoryWarning() {
@@ -48,43 +34,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
-        UserDefaults.standard.setValue(locValue.latitude, forKey: "myCurrentLat")
-        UserDefaults.standard.setValue(locValue.longitude, forKey: "myCurrentLong")
-    }
-    
     @IBAction func logoutClicked(_ sender: UIButton) {
         UserDefaults.standard.removeObject(forKey: "username")
         UserDefaults.standard.removeObject(forKey: "user_id")
     }
-    
-    func copyDatabaseIfNeeded() {
-        // Move database file from bundle to documents folder
-        
-        let fileManager = FileManager.default
-        
-        guard let documentsUrl = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
-        
-        let finalDatabaseURL = documentsUrl.appendingPathComponent("iMenusDB.db")
-        
-        do {
-            if !fileManager.fileExists(atPath: finalDatabaseURL.path) {
-                print("DB does not exist in documents folder")
-                
-                if let dbFilePath = Bundle.main.path(forResource: "iMenusDB", ofType: "db") {
-                    try fileManager.copyItem(atPath: dbFilePath, toPath: finalDatabaseURL.path)
-                } else {
-                    print("Uh oh - iMenusDB.db is not in the app bundle")
-                }
-            } else {
-                print("Database file found at path: \(finalDatabaseURL.path)")
-            }
-        } catch {
-            print("Unable to copy iMenusDB.db: \(error)")
-        }
-    }
-
-
 }
 
